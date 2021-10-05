2578e6126be32608da21321d91c6e5e685e88c51
SalamanderXVIII <70542530+SalamanderXVIII@users.noreply.github.com>
2021-10-05 13:23:10 +0200

    Add files via upload

---
+++ 2nd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2nd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение y: ");
+            double y = double.Parse(Console.ReadLine());
+            double p = Math.Sqrt(x * x + y * y);
+            double f = Math.Atan(x / y) * 57.296;
+            Console.WriteLine("p = {0:F1}", p);
+            Console.WriteLine("Градусы = {0:F1}", f);
+        }
+    }
+}
